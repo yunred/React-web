61600d5a2618fda09297b05e296494abf7e74559
SEOYUN KIM <nado0903@gmail.com>
2021-07-08 18:46:48 +0900

    lotto-game

---
+++ lotto-game/Lotto.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
+import Ball from './Ball';
+
+function getWinNumbers() { //lotto숫자들을 return
+  console.log('getWinNumbers');
+  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber]; //winNumbers는 lotto 당첨 번호들
+}
+
+const Lotto = () => { //선언 순서 중요
+  const lottoNumbers = useMemo(() => getWinNumbers(), []);
+  const [winNumbers, setWinNumbers] = useState(lottoNumbers);
+  const [winBalls, setWinBalls] = useState([]);
+  const [bonus, setBonus] = useState(null);
+  const [redo, setRedo] = useState(false);
+  const timeouts = useRef([]);
+
+  useEffect(() => {
+    console.log('useEffect');
+    for (let i = 0; i < winNumbers.length - 1; i++) {
+      timeouts.current[i] = setTimeout(() => { //이건 timeout.current가 바뀌는게 아님. 배열의 요소에 넣어준거라서
+        setWinBalls((prevBalls) => [...prevBalls, winNumbers[i]]);
+      }, (i + 1) * 1000);
+    }
+    timeouts.current[6] = setTimeout(() => {
+      setBonus(winNumbers[6]);
+      setRedo(true);
+    }, 7000);
+    return () => {//componentWillUnmout 역할
+      timeouts.current.forEach((v) => {
+        clearTimeout(v);
+      });
+    };
+  }, [timeouts.current]); // 빈 배열이면 componentDidMount와 동일
+  // 배열에 요소가 있으면 componentDidUpdate 까지 모두 수행
+
+  //timeouts.current는 onClickRedo가 실행될 때 timeouts.current = [];으로 바뀐다
+  //바뀌는 시점을 배열 요소로 넣으면 됨
+
+  useEffect(() => {
+    console.log('로또 숫자를 생성합니다.');
+  }, [winNumbers]);
+
+  const onClickRedo = useCallback(() => {
+    console.log('onClickRedo');
+    console.log(winNumbers);
+    setWinNumbers(getWinNumbers());
+    setWinBalls([]);
+    setBonus(null);
+    setRedo(false);
+    timeouts.current = [];
+  }, [winNumbers]);
+
+  return (
+    <>
+      <div>당첨 숫자</div>
+      <div id="결과창">
+        {winBalls.map((v) => <Ball key={v} number={v} />)}
+      </div>
+      <div>보너스!</div>
+      {bonus && <Ball number={bonus} onClick={onClickRedo} />}
+      {redo && <button onClick={onClickRedo}>한 번 더!</button>}
+    </>
+  );
+};
+
+export default Lotto;
